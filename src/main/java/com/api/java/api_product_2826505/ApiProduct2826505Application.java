e835fa4e61139cbd451fc0af1c74a21a665d3413
unknown <Aprendiz@BOGDFPCGMS4141.FormacionSena.RED>
2024-07-24 16:27:34 -0500

    entidad creada

---
+++ src/main/java/com/api/java/api_product_2826505/ApiProduct2826505Application.java
@@ -0,0 +1,13 @@
+package com.api.java.api_product_2826505;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProduct2826505Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProduct2826505Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/java/api_product_2826505/ApiProduct2826505ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.java.api_product_2826505;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProduct2826505ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
