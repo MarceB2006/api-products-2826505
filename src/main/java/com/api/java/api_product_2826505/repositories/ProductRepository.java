64e43a0ba201ef3f27c3e38677233a30bae78dc2
unknown <Aprendiz@BOGDFPCGMS4141.FormacionSena.RED>
2024-07-24 17:21:08 -0500

    repositorio creado

---
+++ src/main/java/com/api/java/api_product_2826505/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.api.java.api_product_2826505.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.api.java.api_product_2826505.entities.Product;
+
+public interface ProductRepository extends CrudRepository<Product , Long> {
+//Repositorio por cada entidad 
+
+}
